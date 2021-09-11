50ae6afc1dab89999475482016e861db4a7839c3
AbdulWahab321 <76244094+AbdulWahab321@users.noreply.github.com>
2021-09-11 20:56:49 +0530

    Delete cmpt directory

--- cmpt/pywhatkit/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Pull Requests are always Welcomed"""
-
-from components.cmpt.pywhatkit.ascii_art import image_to_ascii_art
-from components.cmpt.pywhatkit.chr_to_handwriting import text_to_handwriting
-from components.cmpt.pywhatkit.history import show_history
-from components.cmpt.pywhatkit.mail import send_mail, send_hmail
-from components.cmpt.pywhatkit.main import print_sleep_time, check_window, sendwhatmsg, sendwhatmsg_to_group, info, playonyt, search, close_tab, sendwhatmsg_instantly, sendwhats_image
-from components.cmpt.pywhatkit.sc import shutdown, cancel_shutdown
-from components.cmpt.pywhatkit.wwd import tutorial_english, tutorial_hindi, developer_contact, join_discord
